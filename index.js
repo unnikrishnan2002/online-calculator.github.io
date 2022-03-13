267c46c7e761aeb866efb4bc1d12f9de3b995f7f
Unni Krishnan <kvunni2002@gmail.com>
2022-03-13 12:43:55 +0530

    fixed the issue

--- index.js
+++ index.js
@@ -3,17 +3,20 @@ let outputScreen = document.getElementById("output");
 function display(num){
     outputScreen.value += num;
 }
+
+function Clear() {
+    outputScreen.value = "";
+}
+
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
+
 function Calculate(){
     try{
         outputScreen.value = eval(outputScreen.value);
     }
     catch(err){
         alert("never gonna give you up never gonna let you down never gonna tell a lie and hurt you:)")
-    }
-    function Clear(){
-        outputScreen.value = "";
-    }
-    function del(){
-        outputScreen.value = outputScreen.value.slice(0,-1);
     }
 }
