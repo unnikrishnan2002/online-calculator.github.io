f832e494f26e95049a7fdb846fe231a8fc042996
Deepanshu <97582830+DeepanshuDaksh77@users.noreply.github.com>
2022-03-13 11:25:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let outputScreen = document.getElementById("output");
+
+function display(num){
+    outputScreen.value += num;
+}
+function Calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch(err){
+        alert("never gonna give you up never gonna let you down never gonna tell a lie and hurt you:)")
+    }
+    function Clear(){
+        outputScreen.value = "";
+    }
+    function del(){
+        outputScreen.value = outputScreen.value.slice(0,-1);
+    }
+}
